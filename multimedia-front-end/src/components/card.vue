636c50efc8835a00e65c4ee626b2330a84893c0b
xuchunhao <1010549831@qq.com>
2019-09-06 17:25:11 +0800

    add wheel

--- multimedia-front-end/src/components/card.vue
+++ multimedia-front-end/src/components/card.vue
@@ -2,31 +2,31 @@
   <div class="card">
     <ul>
       <router-link to="introduce" tag="li">
-        <span>简介</span>
+        <span>简 介</span>
         <span>Brief Introduction</span>
       </router-link>
       <router-link to="technology" tag="li">
-        <span>工艺流程</span>
+        <span>工 艺 流 程</span>
         <span>Technological Process</span>
       </router-link>
       <router-link to="source" tag="li">
-        <span>溯源</span>
+        <span>溯 源</span>
         <span>Trace to the Source</span>
       </router-link>
       <router-link to="inherit" tag="li">
-        <span>文化传承</span>
+        <span>文 化 传 承</span>
         <span>Cultural Inheritance</span>
       </router-link>
       <li>
-        <span>娱乐互动</span>
+        <span>娱 乐 互 动</span>
         <span>Entertainment</span>
       </li>
       <li>
-        <span>游客论坛</span>
+        <span>游 客 论 坛</span>
         <span>Tourists BBS</span>
       </li>
       <li>
-        <span>关于我们</span>
+        <span>关 于 我 们</span>
         <span>About Us</span>
       </li>
     </ul>
--- multimedia-front-end/src/components/wheel.vue
+++ multimedia-front-end/src/components/wheel.vue
@@ -2,68 +2,84 @@
   <div class="wheel">
     <!-- <div v-if="this.materialList.length>1" class="arrow-left" @click="materialKeyEdit(-1)"><img src="../../assets/image/arrow-left.png" alt=""></div> -->
 
-    <div class="material-scroll" ref="material-scroll" @mousewheel="scrollEvent">
-      <div class="material-left" id="material-scroll">
-        <div
-          class="material-box"
-          v-for="(item,index) in materialList"
-          :key="index"
-          @click="materialKeyEdit(index)"
-          :class="{'blue':index==materialKey}"
-        >
-          <div>
-            <img style="max-width:100%;max-height:100%" class :src="item" alt />
+    <div class="wheel-box">
+      <div class="material-scroll">
+        <div class="material-left" id="material-scroll" ref="material-scroll" @mousewheel="scrollEvent">
+          <div
+            class="material-box"
+            v-for="(item,index) in materialList"
+            :key="index"
+            @click="materialKeyEdit(index)"
+            :class="{'blue':index==materialKey}"
+          >
+            <div> 
+              <div>
+                <img v-if="index == materialKey - 2" style="max-width:100%;max-height:100%" class="first" :src="item" alt />
+              </div>
+              <div>
+                <img v-if="index == materialKey - 1" style="max-width:100%;max-height:100%" class="second" :src="item" alt />
+              </div>
+              <div>
+                <img v-if="index == materialKey" style="max-width:100%;max-height:100%" class="thrid" :src="item" alt />
+              </div>
+              <div>
+                <img v-if="index == materialKey + 1" style="max-width:100%;max-height:100%" class="fourth" :src="item" alt />
+              </div>
+              <div>
+                <img v-if="index == materialKey + 2" style="max-width:100%;max-height:100%" class="fifth" :src="item" alt />
+              </div>
+            </div>
+            <!-- <div v-if="item.match(/((http|https):\/\/)/ig) && item.match(/((.jpg|.png|.gif|.webp))/ig)">
+                <img style="max-width:100%;max-height:100%" class :src="item" alt />
+              </div>
+
+              <div
+                v-else-if="item.match(/((http|https):\/\/)/ig) && item.match(/((.mp4|.rmvb|.rm|.3gp|.mov|.avi))/ig)"
+              >
+                <video style="max-width:100%;max-height:100%" :src="item" alt></video>
+              </div>
+
+            <div style="width:80%" v-else>{{item}}</div>-->
           </div>
-          <!-- <div v-if="item.match(/((http|https):\/\/)/ig) && item.match(/((.jpg|.png|.gif|.webp))/ig)">
-              <img style="max-width:100%;max-height:100%" class :src="item" alt />
-            </div>
-
-            <div
-              v-else-if="item.match(/((http|https):\/\/)/ig) && item.match(/((.mp4|.rmvb|.rm|.3gp|.mov|.avi))/ig)"
-            >
-              <video style="max-width:100%;max-height:100%" :src="item" alt></video>
-            </div>
-
-          <div style="width:80%" v-else>{{item}}</div>-->
         </div>
       </div>
-    </div>
 
-    <div
-      class="material-right"
-      style
-      v-for="(item,index) in materialList"
-      :key="index"
-      v-show="index==materialKey"
-    >
-      <!-- <div class="arrow-left" @click="materialKeyAdd(-1)">
-        <i class="el-icon-arrow-up"></i>
-      </div> -->
+      <div
+        class="material-right"
+        style
+        v-for="(item,index) in materialList"
+        :key="index"
+        v-show="index==materialKey"
+      >
+        <!-- <div class="arrow-left" @click="materialKeyAdd(-1)">
+          <i class="el-icon-arrow-up"></i>
+        </div> -->
 
-      <div>
-        <img class :src="item" alt style="max-height:45vh" />
-      </div>
-      <!-- <div v-if="item.match(/((http|https):\/\/)/ig) && item.match(/((.jpg|.png|.gif|.webp))/ig)">
+        <div>
           <img class :src="item" alt style="max-height:45vh" />
         </div>
+        <!-- <div v-if="item.match(/((http|https):\/\/)/ig) && item.match(/((.jpg|.png|.gif|.webp))/ig)">
+            <img class :src="item" alt style="max-height:45vh" />
+          </div>
 
-        <div
-          v-else-if="item.match(/((http|https):\/\/)/ig) && item.match(/((.mp4|.rmvb|.rm|.3gp|.mov|.avi))/ig)"
-        >
-          <video style="max-height:45vh" :src="item" alt controls="controls"></video>
-        </div>
+          <div
+            v-else-if="item.match(/((http|https):\/\/)/ig) && item.match(/((.mp4|.rmvb|.rm|.3gp|.mov|.avi))/ig)"
+          >
+            <video style="max-height:45vh" :src="item" alt controls="controls"></video>
+          </div>
 
-        <div v-else>{{item}}
-      </div>-->
-      <!-- 
-      <div class="arrow-right" @click="materialKeyAdd(1)">
-        <i class="el-icon-arrow-down"></i>
-      </div> -->
+          <div v-else>{{item}}
+        </div>-->
+        <!-- 
+        <div class="arrow-right" @click="materialKeyAdd(1)">
+          <i class="el-icon-arrow-down"></i>
+        </div> -->
 
-      <!-- <p style="text-align:center">{{index+1}} / {{materialList.length}}</p> -->
-    </div>
+        <!-- <p style="text-align:center">{{index+1}} / {{materialList.length}}</p> -->
+      </div>
 
     <!-- <div v-if="this.materialList.length>1" class="arrow-right" @click="materialKeyEdit(1)"><img src="../../assets/image/arrow-right.png" alt=""></div> -->
+    </div>
   </div>
 </template>
 
@@ -78,7 +94,10 @@ export default {
         require("@/assets/img/home/cloud2.webp"),
         require("@/assets/img/home/cloud3.webp"),
         require("@/assets/img/home/cloud4.webp"),
-        require("@/assets/img/home/cloud1.webp")
+        require("@/assets/img/home/cloud1.webp"),
+        require("@/assets/img/home/cloud2.webp"),
+        require("@/assets/img/home/cloud3.webp"),
+        require("@/assets/img/home/cloud4.webp")
       ],
       materialKey: 0
     };
@@ -88,24 +107,32 @@ export default {
     
     scrollEvent(e){
       if(e.deltaY > 0) {
+        
+        if(this.materialKey > this.materialList.length - 2){
+          //this.materialKey = 0;
+          return
+        }
         this.materialKey++;
         let box = this.$refs["material-scroll"];
-        box.style.marginTop += '75px'; 
-        if(this.materialKey > this.materialList.length - 1){
-          this.materialKey = 1;
-        }
-
+        let px = box.style.marginTop;
+        let height = Number(px.substring(0, px.length - 2));
+        box.style.marginTop = height - 75 + 'px'; 
+        console.log(1)
       }
       if(e.deltaY < 0) {
+        if(this.materialKey <= 0){
+          //this.materialKey = this.materialList.length;
+          return
+        }
         this.materialKey--;
         let box = this.$refs["material-scroll"];
-        box.style.transform = "translateY(" + -75 + "px)";
-        if(this.materialKey <= 0){
-          this.materialKey = this.materialList.length - 1;
-        }
+        let px = box.style.marginTop;
+        let height = Number(px.substring(0, px.length - 2));
+        box.style.marginTop =   height + 75 + 'px'; 
+        console.log(2)
       }
       
-      console.log(e)
+      // console.log(e)
       // if(e.srcElement.scrollTop+e.srcElement.offsetHeight>e.srcElement.scrollHeight-100 && !this.moreLoading && !this.noMore){
       //   alert(111)
       // }
@@ -161,94 +188,5 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-
-.material-scroll {
-  //滚动div
-  display: flex;
-  height: 70vh;
-  justify-content: center;
-  align-items: center;
-  float: left;
-  width: 25%;
-  overflow-y: auto;
-  // background: #ddd;
-}
-
-.material-left {
-  width: 80%;
-  max-height: 70vh;
-  //  overflow-y: auto;
-  //  display:flex;
-  //  justify-content: center;
-  //  align-items: center;
-
-  //  float: left;
-  //  max-height: 300px;
-  .material-box {
-    margin-bottom: 5px;
-
-    > div {
-      cursor: pointer;
-      display: flex;
-      justify-content: center;
-      align-items: center;
-      height: 70px;
-      width: 95%;
-      // border: 1px solid #ddd;
-      //background: #fff;
-      overflow: hidden;
-    }
-  }
-
-}
-.material-right {
-  height: 70vh;
-  text-align: center;
-  margin-left: 26%;
-  position: relative;
-  margin-right: 10px;
-  // display: flex;
-  // justify-content: center;
-  // align-items: center;
-
-  > div {
-    height: 66vh;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-  }
-
-  .arrow-left {
-    position: absolute;
-    top: -0%;
-    left: 2%;
-    cursor: pointer;
-    // background: #dcdfe6;
-    width: 95%;
-    height: 6vh;
-    font-size: 50px;
-    font-weight: bold;
-  }
-  .arrow-right {
-    position: absolute;
-    bottom: 5%;
-    left: 2%;
-    cursor: pointer;
-    // background: #dcdfe6;
-    width: 95%;
-    height: 6vh;
-    font-size: 50px;
-    font-weight: bold;
-  }
-  .arrow-left:hover {
-    background: #dcdfe6;
-    opacity: 0.5;
-    color: #fff;
-  }
-  .arrow-right:hover {
-    background: #dcdfe6;
-    opacity: 0.5;
-    color: #fff;
-  }
-}
+  @import '@/assets/css/wheel.scss'
 </style>
