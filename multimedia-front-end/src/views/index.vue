e2ea028ad29feb6673f4badd96548322f3bf35f7
Kr328 <kr328@users.noreply.github.com>
2019-09-17 12:15:34 +0800

    home page resize

--- multimedia-front-end/src/views/index.vue
+++ multimedia-front-end/src/views/index.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="home">
+  <div id="content-background">
     <!-- <el-row type="flex" justify="center" class="test">
       <el-col :span="5" class="test1">
         <el-row type="flex" justify="space-between">
@@ -17,25 +17,23 @@
         <img src="~@/assets/img/home/logo.png" alt="" class="logo">
       </el-col>
     </el-row>-->
-    <div class="content">
-      <div id="footer-container">
-        <div id="center-content">
-          <div class="box">
-            <img src="~@/assets/img/home/title.webp" alt class="title" draggable="false" />
-            <img src="~@/assets/img/home/seal.webp" alt class="seal" draggable="false" />
-          </div>
-          <div class="logo-content">
-            <img src="~@/assets/img/home/logo.webp" alt class="logo" draggable="false" />
-          </div>
+    <div id="content">
+      <div id="center-content">
+        <div id="content-title">
+          <img src="~@/assets/img/home/title.webp" alt id="title" draggable="false" />
+          <img src="~@/assets/img/home/seal.webp" alt id="seal" draggable="false" />
         </div>
-        <router-link to="/navbar">
-          <img src="~@/assets/img/home/btn-threeline.png" alt class="btn-threeline" />
-        </router-link>
-        <!-- <img draggable="false" src="~@/assets/img/home/cloud1.webp" alt class="cloud1" />
+        <div id="content-logo">
+          <img src="~@/assets/img/home/logo.webp" alt id="logo" draggable="false" />
+        </div>
+      </div>
+      <router-link to="/navbar">
+        <img src="~@/assets/img/home/btn-threeline.png" alt class="btn-threeline" />
+      </router-link>
+      <!-- <img draggable="false" src="~@/assets/img/home/cloud1.webp" alt class="cloud1" />
     <img draggable="false" src="~@/assets/img/home/cloud2.webp" alt class="cloud2" />
     <img draggable="false" src="~@/assets/img/home/cloud3.webp" alt class="cloud3" />
-        <img draggable="false" src="~@/assets/img/home/cloud4.webp" alt class="cloud4" />-->
-      </div>
+      <img draggable="false" src="~@/assets/img/home/cloud4.webp" alt class="cloud4" />-->
     </div>
   </div>
 </template>
